0449c0c660be7c4d2e31aaf6c48a4d49db658d8c
Andrew Zenk <azenk@umn.edu>
2019-05-26 21:47:48 -0500

    Implement basic client functionality for IPAM endpoint

--- go.mod
+++ go.mod
@@ -3,20 +3,27 @@ module github.com/PolarGeospatialCenter/inventory-client
 go 1.12
 
 require (
-	github.com/PolarGeospatialCenter/inventory v0.1.0
+	github.com/Microsoft/go-winio v0.4.12 // indirect
+	github.com/PolarGeospatialCenter/dockertest v0.0.0-20190402172603-7e70c31421a4 // indirect
+	github.com/PolarGeospatialCenter/inventory v0.2.0
 	github.com/PolarGeospatialCenter/vaulthelper v0.0.0-20190213212614-2c029db3511b
 	github.com/aws/aws-lambda-go v1.9.0 // indirect
 	github.com/aws/aws-sdk-go v1.18.4
 	github.com/azenk/iputils v0.0.0-20180901170612-d1883c0677d3 // indirect
-	github.com/golang/snappy v0.0.1 // indirect
-	github.com/hashicorp/go-retryablehttp v0.5.2 // indirect
-	github.com/hashicorp/go-rootcerts v1.0.0 // indirect
-	github.com/hashicorp/go-sockaddr v1.0.2 // indirect
-	github.com/hashicorp/vault v1.0.3
-	github.com/pierrec/lz4 v2.0.5+incompatible // indirect
-	github.com/ryanuber/go-glob v1.0.0 // indirect
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/docker v1.13.1 // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.3.3 // indirect
+	github.com/go-test/deep v1.0.2-0.20181118220953-042da051cf31 // indirect
+	github.com/hashicorp/vault/api v1.0.2
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
 	github.com/spf13/viper v1.3.2
-	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
+	golang.org/x/net v0.0.0-20190522155817-f3200d17e092 // indirect
+	golang.org/x/sys v0.0.0-20190526052359-791d8a0f4d09 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/h2non/gock.v1 v1.0.14
 	gopkg.in/resty.v1 v1.12.0
 )
---
+++ pkg/api/client/ipam_test.go
@@ -0,0 +1,180 @@
+package client
+
+import (
+	"net"
+	"net/http"
+	"net/url"
+	"testing"
+
+	"github.com/PolarGeospatialCenter/inventory/pkg/inventory/types"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	gock "gopkg.in/h2non/gock.v1"
+)
+
+func TestIPReservationGet(t *testing.T) {
+	gock.DisableNetworking()
+	defer gock.EnableNetworking()
+	defer gock.Off()
+	testBaseUrl, _ := url.Parse("https://inventory.api.local/v0/")
+
+	gock.New(testBaseUrl.String()).
+		Get("ipam/ip/10.0.0.1").
+		Reply(http.StatusOK).
+		BodyString(`{"mac": "00:01:02:03:04:05","ip": "10.0.0.1/24","start": null,"end": null}`)
+
+	reservation, err := NewInventoryApi(testBaseUrl, &aws.Config{Credentials: credentials.NewStaticCredentials("id", "secret", "token")}).IPAM().GetIPReservation(net.ParseIP("10.0.0.1"))
+	if err != nil {
+		t.Errorf("unable to get ip reservation: %v", err)
+	}
+
+	if reservation.MAC.String() != "00:01:02:03:04:05" {
+		t.Errorf("mac address doesn't match expected value")
+	}
+	if reservation.Start != nil {
+		t.Errorf("expected nil start time")
+	}
+}
+
+func TestIPReservationCreateSpecifiedIP(t *testing.T) {
+	gock.DisableNetworking()
+	defer gock.EnableNetworking()
+	defer gock.Off()
+	testBaseUrl, _ := url.Parse("https://inventory.api.local/v0/")
+
+	gock.New(testBaseUrl.String()).
+		Post("ipam/ip/10.0.0.2").
+		Reply(http.StatusCreated).
+		BodyString(`{
+			"HostInformation": "",
+			"start": "2019-05-27T02:22:30.97489765Z",
+			"end": null,
+			"ip": "10.0.0.2/24",
+			"mac": "00:01:02:03:04:06",
+			"gateway": "10.0.0.1",
+			"dns": [
+				"8.8.8.8",
+				"1.1.1.1"
+			]
+		}`)
+
+	mac, _ := net.ParseMAC("00:01:02:03:04:06")
+
+	new := &types.IpamIpRequest{
+		HwAddress: mac.String(),
+		Subnet:    net.ParseIP("10.0.0.0").String(),
+	}
+	reservation, err := NewInventoryApi(testBaseUrl, &aws.Config{Credentials: credentials.NewStaticCredentials("id", "secret", "token")}).IPAM().CreateIPReservation(new, net.ParseIP("10.0.0.2"))
+	if err != nil {
+		t.Fatalf("unable to get ip reservation: %v", err)
+	}
+
+	if reservation.MAC.String() != "00:01:02:03:04:06" {
+		t.Errorf("mac address doesn't match expected value")
+	}
+	if reservation.Start == nil {
+		t.Errorf("expected non-nil start time")
+	}
+}
+
+func TestIPReservationCreate(t *testing.T) {
+	gock.DisableNetworking()
+	defer gock.EnableNetworking()
+	defer gock.Off()
+	testBaseUrl, _ := url.Parse("https://inventory.api.local/v0/")
+
+	gock.New(testBaseUrl.String()).
+		Post("ipam/ip").
+		Reply(http.StatusCreated).
+		BodyString(`{
+			"HostInformation": "",
+			"start": "2019-05-27T02:22:30.97489765Z",
+			"end": null,
+			"ip": "10.0.0.2/24",
+			"mac": "00:01:02:03:04:06",
+			"gateway": "10.0.0.1",
+			"dns": [
+				"8.8.8.8",
+				"1.1.1.1"
+			]
+		}`)
+
+	mac, _ := net.ParseMAC("00:01:02:03:04:06")
+
+	new := &types.IpamIpRequest{
+		HwAddress: mac.String(),
+		Subnet:    net.ParseIP("10.0.0.0").String(),
+	}
+	reservation, err := NewInventoryApi(testBaseUrl, &aws.Config{Credentials: credentials.NewStaticCredentials("id", "secret", "token")}).IPAM().CreateIPReservation(new, nil)
+	if err != nil {
+		t.Fatalf("unable to get ip reservation: %v", err)
+	}
+
+	if reservation.MAC.String() != "00:01:02:03:04:06" {
+		t.Errorf("mac address doesn't match expected value")
+	}
+	if reservation.Start == nil {
+		t.Errorf("expected non-nil start time")
+	}
+}
+func TestIPReservationUpdate(t *testing.T) {
+	gock.DisableNetworking()
+	defer gock.EnableNetworking()
+	defer gock.Off()
+	testBaseUrl, _ := url.Parse("https://inventory.api.local/v0/")
+
+	gock.New(testBaseUrl.String()).
+		Put("ipam/ip/10.0.0.1").
+		Reply(http.StatusOK).
+		BodyString(`{"mac": "00:01:02:03:04:06","ip": "10.0.0.1/24","start": null,"end": null}`)
+
+	mac, _ := net.ParseMAC("00:01:02:03:04:06")
+	modified := &types.IPReservation{
+		IP:  &net.IPNet{IP: net.ParseIP("10.0.0.1"), Mask: net.IPv4Mask(0xff, 0xff, 0xff, 0x00)},
+		MAC: mac,
+	}
+	reservation, err := NewInventoryApi(testBaseUrl, &aws.Config{Credentials: credentials.NewStaticCredentials("id", "secret", "token")}).IPAM().UpdateIPReservation(modified)
+	if err != nil {
+		t.Fatalf("unable to get ip reservation: %v", err)
+	}
+
+	if reservation.MAC.String() != "00:01:02:03:04:06" {
+		t.Errorf("mac address doesn't match expected value")
+	}
+	if reservation.Start != nil {
+		t.Errorf("expected nil start time")
+	}
+}
+func TestIPReservationDelete(t *testing.T) {
+	gock.DisableNetworking()
+	defer gock.EnableNetworking()
+	defer gock.Off()
+	testBaseUrl, _ := url.Parse("https://inventory.api.local/v0/")
+
+	gock.New(testBaseUrl.String()).
+		Delete("ipam/ip/10.0.0.1").
+		Reply(http.StatusOK)
+
+	reservation := &types.IPReservation{IP: &net.IPNet{IP: net.ParseIP("10.0.0.1"), Mask: net.IPv4Mask(0xff, 0xff, 0xff, 0x00)}}
+	err := NewInventoryApi(testBaseUrl, &aws.Config{Credentials: credentials.NewStaticCredentials("id", "secret", "token")}).IPAM().DeleteIPReservation(reservation)
+	if err != nil {
+		t.Errorf("unable to delete ip reservation: %v", err)
+	}
+}
+func TestIPReservationDeleteNotFound(t *testing.T) {
+	gock.DisableNetworking()
+	defer gock.EnableNetworking()
+	defer gock.Off()
+	testBaseUrl, _ := url.Parse("https://inventory.api.local/v0/")
+
+	gock.New(testBaseUrl.String()).
+		Delete("ipam/ip/10.0.0.1").
+		Reply(http.StatusNotFound).
+		BodyString(`{"status": "Not Found", "error": "object not found"}`)
+
+	reservation := &types.IPReservation{IP: &net.IPNet{IP: net.ParseIP("10.0.0.1"), Mask: net.IPv4Mask(0xff, 0xff, 0xff, 0x00)}}
+	err := NewInventoryApi(testBaseUrl, &aws.Config{Credentials: credentials.NewStaticCredentials("id", "secret", "token")}).IPAM().DeleteIPReservation(reservation)
+	if err == nil {
+		t.Errorf("no error reported when deleting non-existent record")
+	}
+}
